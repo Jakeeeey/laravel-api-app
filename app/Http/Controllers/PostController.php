130530e0321434412664765595072b6192ab232f
Jakeeeey <jakedavedeguzman123@gmail.com>
2025-03-19 01:20:20 +0800

    update

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -15,7 +15,8 @@ class PostController extends Controller
      */
     public function index()
     {
-        return Post::all();
+        return 'Hi';
+        //return Post::all();
     }
 
     /**
